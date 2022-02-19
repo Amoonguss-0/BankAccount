0a6d7b157f03838c0f418b6d247e7a6279c0e060
Justin-F5972 <justinfernandez27@outlook.com>
2022-02-18 21:13:11 -0800

    Starting to implement withdraw into unit testing

--- BankAccountTests/AccountTests.cs
+++ BankAccountTests/AccountTests.cs
@@ -11,25 +11,77 @@ namespace BankAccount.Tests
     [TestClass()]
     public class AccountTests
     {
-        [TestMethod()]
-        public void Desposit_APositiveAmount_A()
-        {
-            Account acc = new Account("J. Doe");
-            acc.Deposit(100);
+        private Account acc;
 
-            Assert.AreEqual(100, acc.Balance);
+        [TestInitialize]
+        // This needs to be public
+        public void CreateDefaultAccount()
+        {
+            acc = new Account("J. Doe");
+        }
+
+        [TestMethod()]
+        [DataRow(100)]
+        [DataRow(.01)]
+        [DataRow(1.99)]
+        [DataRow(9_999.99)]
+        public void Desposit_APositiveAmount_A(double depositAmount)
+        {
+            // Arrange
+            acc.Deposit(depositAmount);
+            // Assert
+            Assert.AreEqual(depositAmount, acc.Balance);
         }
 
         [TestMethod]
-        public void Despoit_APositiveAmount_ReturnsUpdatedBalance()
+        public void Desposit_APositiveAmount_ReturnsUpdatedBalance()
         {
-            Account acc = new Account("J. Doe");
+            // Arrange
             double depositAmount = 100; 
             double expectedReturn = 100;
 
+            // Act
             double returnValue = acc.Deposit(depositAmount);
 
+            // Assert
             Assert.AreEqual(expectedReturn, returnValue);
+        }
+
+        [TestMethod]
+        [DataRow(-1)]
+        [DataRow(0)]
+        public void Deposit_ZeroOrLess_ThrowsArgumentException(double invalidDepositAmount)
+        {
+            // Arrange
+            // Nothing to add
+
+            // Assert => Act
+            Assert.ThrowsException<ArgumentOutOfRangeException>
+                (() => acc.Deposit(invalidDepositAmount));
+        }
+
+        // Withdrawing a positive amount
+        // Withdrawing 0 - throws ArgumentRangeException
+        // Withdrawing negative amount - throws ArgumentOutRange exception
+        // Withdrawing more than available balance ArgumentException
+
+        [TestMethod]
+        public void Withdraw_PositiveAmount_DecreasesBalance()
+        {
+            // Arrange
+            double initialDeposit = 100;
+            double withDrawalAmount = 50;
+            double expectedBalance = initialDeposit - withDrawalAmount;
+
+            // Act
+            acc.Deposit(initialDeposit);
+            acc.Withdraw(withDrawalAmount);
+
+            double actualBalance = acc.Balance;
+
+            // Assert
+            Assert.AreEqual(expectedBalance, actualBalance);
+
         }
     }
 }
